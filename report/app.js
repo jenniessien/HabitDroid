b4f6d47d2bcb47e4e27bdc586f9c3f38872fc6f9
Anita Amini <baadge29@yahoo.co.uk>
2015-03-07 16:28:42 +0000

    d3

---
+++ report/app.js
@@ -0,0 +1,31 @@
+//$(document ).ready(function() {
+//        $("#button").click(function() {
+//        
+//    });
+//});
+//
+//
+//AJAX
+//
+//var fetchData = function(){
+//                    $.ajax({
+//                        // url: "http://localhost:8000/tweets",
+//                        url: "https://jenni.gif/log_",
+//                        success: function(data) {
+//                            var temp = JSON.parse(data);
+//
+//                            blinkArray = temp.blinkArray;
+//                            slouchArray = temp.slouchArray;
+//
+//                            console.log("update!");
+//
+//                            
+//                            }
+//
+//                            updateBars();
+//                        }
+//                    })
+//                    setTimeout(fetchData, 5000);	
+//                };
+//
+//                fetchData();
