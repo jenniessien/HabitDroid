e1843c3e85d39188c94983276cce29593f34bd97
Jennifer Naiaretti <j.naiaretti@gmail.com>
2015-03-07 17:58:40 +0000

    json encoded

--- report/generate/index.php
+++ report/generate/index.php
@@ -6,7 +6,7 @@ include_once('config.php');
 logReports();
 
 	function logReports() {
-		echo "This script will run every minute.";
+		echo "This script will run every minute.....";
 
 		// find all blinks/movements/slouches within the hour just past. When cron runs at 14:00:00 Get count for 13:00:00 - 13:59:59 and delete them
 		$db_handle = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
@@ -14,22 +14,13 @@ logReports();
 		    die("Connection failed: " . $conn->connect_error);
 		}
 
+		logBlinksCount($db_handle);
+		// logMovementsCount($db_handle);
+		// logSlouchesCount($db_handle);
+
 		// Get the most recent count from the tables
 		// Get the hour it was done within
-		$getLastBlinkReportQuery = "SELECT `created` FROM blinks ORDER BY `created` DESC LIMIT 1";
-		$lastBlinkRecordResult = $db_handle->query($getLastBlinkReportQuery);
 
-		if ($lastBlinkRecordResult) {
-			$numRows = mysqli_num_rows($lastBlinkRecordResult);
-
-			if ($numRows > 0) {
-				$result = $lastBlinkRecordResult->fetch_assoc();
-			} else {
-				$result = 0;
-			}
-		}
-
-		echo $result;
 
 		// $findTotalBlinksQuery = "SELECT * FROM events WHERE ";
 
@@ -41,4 +32,65 @@ logReports();
 		// $js_array = json_encode($php_array);
 		// echo "var javascript_array = ". $js_array . ";\n";
 	}
+
+	function logBlinksCount($db_handle) {
+		$generateBlinkReportQuery = "SELECT hour(`time`), count(*) FROM events WHERE `time` > now() - interval 24 hour AND type = 'blink' GROUP BY hour(`time`);";
+
+		// $getLastBlinkReportQuery = "SELECT `created` FROM blinks ORDER BY `created` DESC LIMIT 1";
+		$lastBlinkRecordResult = $db_handle->query($generateBlinkReportQuery);
+		$results = array();
+
+		if ($lastBlinkRecordResult) {
+			$numRows = mysqli_num_rows($lastBlinkRecordResult);
+
+			if ($numRows > 0) {
+				while ($row = $lastBlinkRecordResult->fetch_assoc()) {
+					$results[] = $row;
+				}
+			}
+		}
+
+		foreach ($results as $key => $value) {
+			foreach ($value as $count => $hour) {
+				echo $count;
+				echo $hour; die;
+				$saveCountQuery = "";
+			}
+		}
+		// var_dump($results);
+	}
+
+	// function logMovementsCount($db_handle) {
+	// 	$getLastMovementReportQuery = "SELECT `created` FROM blinks ORDER BY `created` DESC LIMIT 1";
+	// 	$lastBlinkRecordResult = $db_handle->query($getLastMovementReportQuery);
+
+	// 	if ($lastBlinkRecordResult) {
+	// 		$numRows = mysqli_num_rows($lastBlinkRecordResult);
+
+	// 		if ($numRows > 0) {
+	// 			$result = $lastBlinkRecordResult->fetch_assoc();
+	// 		} else {
+	// 			$result = 0;
+	// 		}
+	// 	}
+
+	// 	echo $result;
+	// }
+
+	// function logSlouchesCount($db_handle) {
+	// 	$getLastSlouchReportQuery = "SELECT `created` FROM blinks ORDER BY `created` DESC LIMIT 1";
+	// 	$lastBlinkRecordResult = $db_handle->query($getLastSlouchReportQuery);
+
+	// 	if ($lastBlinkRecordResult) {
+	// 		$numRows = mysqli_num_rows($lastBlinkRecordResult);
+
+	// 		if ($numRows > 0) {
+	// 			$result = $lastBlinkRecordResult->fetch_assoc();
+	// 		} else {
+	// 			$result = 0;
+	// 		}
+	// 	}
+
+	// 	echo $result;
+	// }
 ?>
