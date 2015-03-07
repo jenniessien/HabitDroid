997d9f8bd4c04c4831726c2dffa808be706b21d3
riyanat <riyanat.shittu@yahoo.co.uk>
2015-03-07 14:55:32 +0000

    only main with links

--- src/hack/facebook/habitdroid/Main.java
+++ src/hack/facebook/habitdroid/Main.java
@@ -3,6 +3,7 @@ package hack.facebook.habitdroid;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.UnsupportedEncodingException;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -41,7 +42,16 @@ public class Main {
 	    {
 	        public void actionPerformed(ActionEvent e)
 	        {
-	           // Launch new frame for eye detector.
+	           Face faceDetection = new Face();
+	           try {
+				faceDetection.run();
+			} catch (UnsupportedEncodingException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 	        }
 	    });
 	    
