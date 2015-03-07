e46df32753001d3bcdab86eb80937ad936baa9df
Anita Amini <baadge29@yahoo.co.uk>
2015-03-07 17:03:38 +0000

    ajax call

--- report/app.js
+++ report/app.js
@@ -1,31 +1,18 @@
-//$(document ).ready(function() {
-//        $("#button").click(function() {
-//        
-//    });
-//});
-//
-//
-//AJAX
-//
-//var fetchData = function(){
-//                    $.ajax({
-//                        // url: "http://localhost:8000/tweets",
-//                        url: "https://jenni.gif/log_",
-//                        success: function(data) {
-//                            var temp = JSON.parse(data);
-//
+var fetchData = function(){
+                    $.ajax({
+                        url: "http://jenni.gift/get_report.php",
+                        success: function(data) {
+                            var temp = JSON.parse(data);
+
 //                            blinkArray = temp.blinkArray;
 //                            slouchArray = temp.slouchArray;
-//
-//                            console.log("update!");
-//
-//                            
-//                            }
-//
-//                            updateBars();
-//                        }
-//                    })
-//                    setTimeout(fetchData, 5000);	
-//                };
-//
-//                fetchData();
+
+                            console.log(data);
+
+                            
+                            }
+                    });
+                   // setTimeout(fetchData, 5000);	
+                };
+
+                
